3e8dd1a82ef0d3a3f8045284ad6355ef80b27003
dmitrosh <dmytrosh@default-value.com>
2019-11-30 00:30:53 +0200

    - Add Create Method

--- src/Controller/ToDoListController.php
+++ src/Controller/ToDoListController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ToDoListController extends AbstractController
@@ -13,5 +15,28 @@ class ToDoListController extends AbstractController
     public function index()
     {
         return $this->render('index.html.twig');
+    }
+
+    /**
+     * @Route("/create", name="create_task", methods={"POST"})
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function create(Request $request)
+    {
+        if (empty($title = trim($request->request->get('title'))))
+            return $this->redirectToRoute('to_do_list');
+
+        $request->request->get('title');
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $task = new Task();
+        $task->setTitle($title);
+
+        $entityManager->persist($task);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('to_do_list');
     }
 }
